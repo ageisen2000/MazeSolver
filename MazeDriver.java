bba58d10540f6ba3093fb5f67120c27dd2857c1a
Adam <adam.geisen@gmail.com>
2016-01-05 16:24:36 -0600

    Create MazeDriver.java

---
+++ MazeDriver.java
@@ -0,0 +1,11 @@
+package maze;
+
+public class MazeDriver {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Maze maze = new Maze();
+        System.out.println("Test:");
+        maze.printMaze();
+    }
+}
